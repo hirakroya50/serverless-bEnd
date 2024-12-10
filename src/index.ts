42e019307db74886d3ca44b2bda0c9fc2c5497f6
hirakroya50 <hirakroya50@gmail.com>
2024-12-10 20:06:42 +0530

    add prisma pool in hono

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,47 @@
 import { Hono } from "hono";
 import { authMiddleWare } from "./middlewares/auth";
+import { getPrisma } from "./prisma-connect/prismaFunction";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+  };
+}>();
+
 app.post("/", async (c) => {
-  //db call
-  // get req body , headers , query params , meddilewires
-  const body = await c.req.json();
-  console.log("body--", body);
-  console.log(c.req.header("Authorization"));
-  console.log(c.req.query("params"));
-  const params = c.req.query("params");
-  return c.json({
-    status: "on1",
-    body,
-    params: params,
-  });
+  try {
+    const body = await c.req.json();
+    // console.log("body--", body);
+    // console.log(c.req.header("Authorization"));
+    // console.log(c.req.query("params"));
+    // const params = c.req.query("params");
+    const prisma = getPrisma(c.env.DATABASE_URL);
+
+    if (!body.postname)
+      return c.json({
+        status: "failed",
+        msg: "please provide postname in the req body",
+      });
+
+    const post = await prisma.post.create({
+      data: {
+        name: body.postname,
+      },
+    });
+    return c.json({
+      status: 1,
+      post,
+    });
+  } catch (error) {}
 });
-app.get("/", authMiddleWare, (c) => {
-  return c.text("Hello Hono!--------get");
+app.get("/", async (c) => {
+  try {
+    const prisma = getPrisma(c.env.DATABASE_URL);
+    const post = await prisma.post.findMany();
+    return c.json({ post });
+  } catch (error) {
+    console.log(error);
+  }
 });
 app.delete("/", (c) => {
   return c.text("Hello Hono!--------delete");
